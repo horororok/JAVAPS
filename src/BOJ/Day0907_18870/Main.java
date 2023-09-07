34ec424d3f9c54615f7ed57ab8f39dd4ea1ad92b
kth32105 <kth32105@naver.com>
2023-09-07 18:01:57 +0900

    0907

---
+++ src/BOJ/Day0907_18870/Main.java
@@ -0,0 +1,29 @@
+package BOJ.Day0907_18870;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		long [] arr = new long[N];
+		for(int i = 0; i<N; i++) {
+			arr[i] = sc.nextLong();
+		}
+		
+		long arr2[] = arr.clone();
+		
+		Arrays.sort(arr2);
+		
+		
+		
+		
+		
+		
+	}
+}
